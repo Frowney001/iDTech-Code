f75e9e40477d27a5997eff48c58d705235b6b221
frown <frown@GameSlayer>
2016-06-01 22:01:53 -0400

    Initial Upload

---
+++ FunWithVariables/src/FunWithVariables.java
@@ -0,0 +1,18 @@
+
+public class FunWithVariables {
+
+	public static void main(String[] args) {
+		int integer1 = 4;
+		int integer2 = 5;
+		int integer3 = 9;
+		
+		double double1 = 1.5;
+		double double2 = 3.14159;
+		
+		boolean boolean1 = true;
+		boolean boolean2 = false;
+		
+
+	}
+
+}
